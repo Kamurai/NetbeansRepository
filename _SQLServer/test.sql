4a683a7ead302c44e0686d5a649b1a8617885ae0
Kamurai <16156165+Kamurai@users.noreply.github.com>
2023-04-05 02:01:05 +0000

    Update test.sql

--- _SQLServer/test.sql
+++ _SQLServer/test.sql
@@ -1,30 +1,30 @@
---drop table [[Test]].dbo.Colors
+--drop table [HTKB].dbo.Colors
 
 create table [Test].dbo.Colors (
-	ColorIndex		bigint IDENTITY(0,1) PRIMARY KEY,
+	ColorIndex		bigint IDENTITY(0,1) PRIMARY KEY
 	Color					varchar(max) not null
 );
 
-INSERT INTO [Test].dbo.Colors (Color) 
+INSERT INTO [Test].dbo.Users (Color) 
 VALUES ('SQL Server');
 
-INSERT INTO [Test].dbo.Colors (Color) 
+INSERT INTO [Test].dbo.Users (Color) 
 VALUES ('Red');
 
-INSERT INTO [Test].dbo.Colors (Color) 
+INSERT INTO [Test].dbo.Users (Color) 
 VALUES ('Orange');
 
-INSERT INTO [Test].dbo.Colors (Color) 
+INSERT INTO [Test].dbo.Users (Color) 
 VALUES ('Yellow');
 
-INSERT INTO [Test].dbo.Colors (Color) 
+INSERT INTO [Test].dbo.Users (Color) 
 VALUES ('Green');
 
-INSERT INTO [Test].dbo.Colors (Color) 
+INSERT INTO [Test].dbo.Users (Color) 
 VALUES ('Blue');
 
-INSERT INTO [Test].dbo.Colors (Color) 
+INSERT INTO [Test].dbo.Users (Color) 
 VALUES ('Indigo');
 
-INSERT INTO [Test].dbo.Colors (Color) 
+INSERT INTO [Test].dbo.Users (Color) 
 VALUES ('Violet');
